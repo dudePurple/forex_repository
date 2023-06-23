a06ee1aa7ce2221e6cc319e547bb11466b05b454
dudePurple <137522791+dudePurple@users.noreply.github.com>
2023-06-23 20:26:26 +0300

    Update chatgpt_helper.py

--- chatgpt_helper.py
+++ chatgpt_helper.py
@@ -6,7 +6,7 @@ from settings import DEFAULT_ANSWER
 openai.api_key = os.getenv('OPENAI_API_KEY')
 messages = [
     {"role": "system",
-     "content": "You are an intelligent assistant."}
+     "content": "Answer the question."}
 ]
 
 
@@ -16,15 +16,16 @@ def get_answer(question: str):
         return reply
 
     try:
-       chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        messages.append({"role": "user", "content": question})
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
         if (
-            hasattr(chat, 'choices')
-            and chat.choices
-            and hasattr(chat.choices[0], 'message')
-            and hasattr(chat.choices[0].message, 'content')
+                hasattr(chat, 'choices')
+                and chat.choices
+                and hasattr(chat.choices[0], 'message')
+                and hasattr(chat.choices[0].message, 'content')
         ):
             reply = chat.choices[0].message.content
-            
+
     except openai.error.OpenAIError as e:
         # Handle OpenAI API errors
         logging.info("OpenAI API error:", str(e))
